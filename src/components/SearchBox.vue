09b823a189c4c1ccd5acda5b9793d09245b6b40d
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2020-05-04 17:04:41 +0300

    Replace HelloWorld with SearchBox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="search-box container"></div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
